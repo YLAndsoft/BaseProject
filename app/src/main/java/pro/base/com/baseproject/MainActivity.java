83754b4e24ec0ae9fde2b7fae261f1921dc209dd
YLAndsoft <347933430@qq.com>
2018-01-06 11:28:15 +0800

    更新2

--- app/src/main/java/pro/base/com/baseproject/MainActivity.java
+++ app/src/main/java/pro/base/com/baseproject/MainActivity.java
@@ -1,15 +1,44 @@
 package pro.base.com.baseproject;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends Activity {
+import base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    public void initParms(Bundle parms) {
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public View bindView() {
+        return null;
+    }
 
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+
+    @Override
+    public void widgetClick(View v) {
 
     }
 }
